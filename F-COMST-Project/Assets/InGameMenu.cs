23ad5911d56a6e3eca0aaa4d19f2eb655e4cc325
Lakselord <frederik@hasselholm.dk>
2021-11-01 15:16:14 +0100

    start menu and in-game menu

---
+++ F-COMST-Project/Assets/InGameMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject InGameMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    public void Resume()
+    {
+        InGameMenuUI.SetActive(false);
+        Time.timeScale = 1F;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        InGameMenuUI.SetActive(true);
+        Time.timeScale = 0F;
+        GameIsPaused = true;
+    }
+
+    public void LoadStartMenu()
+    {
+        Debug.Log("Loading start menu");
+        Time.timeScale = 1F;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+}
