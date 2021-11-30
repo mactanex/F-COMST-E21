aa4f5b02d00474ee3155dcc7b4e8ceece956f10a
Lakselord <frederik@hasselholm.dk>
2021-11-30 11:19:24 +0100

    Updated TakeDamage

--- F-COMST-Project/Assets/Scripts/Mask.cs
+++ F-COMST-Project/Assets/Scripts/Mask.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Mask : MonoBehaviour
 {
     public int maskNumber;
+    Vector3 originalPosition;
 
     public int GetMaskNumber()
     {
         return maskNumber;
+    }
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        
     }
 }
