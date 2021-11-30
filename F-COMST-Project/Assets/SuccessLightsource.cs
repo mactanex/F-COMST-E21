3a12d4d4660b891ec5213c2f06c7bbbe7ec79daa
Janjaweeds <Villadsen67@hotmail.com>
2021-11-30 14:19:28 +0100

    light sjov

---
+++ F-COMST-Project/Assets/SuccessLightsource.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuccessLightsource : MonoBehaviour
+{
+    Light source; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<Light>();
+        source.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(source.color == Color.red)
+            {
+                source.color = Color.green;
+            } else
+            {
+                source.color = Color.red;
+            }
+        }
+    }
+}
