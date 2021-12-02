b94fe9ba60bde85195a7c98dcb577df59d7bfa2b
mactanex <malthedj@hotmail.com>
2021-12-02 11:28:02 +0100

    added basic sounds to player

--- F-COMST-Project/Assets/Scripts/Controller.cs
+++ F-COMST-Project/Assets/Scripts/Controller.cs
@@ -20,23 +20,22 @@ public class Controller : MonoBehaviour
     public float crouchSmooth = 10f;
 
     Vector3 lastVelocity = Vector3.zero;
+
     [Header("Audio")]
-    public AudioClip[] Clips;
+
     public float PitchMin = 1.0f;
     public float PitchMax = 1.0f;
-    public AudioSource source => m_Source;
-    AudioSource m_Source;
-    public AudioClip FootstepPlayer;
-    public AudioClip JumpingAudioCLip;
-    public AudioClip LandingAudioClip;
+    public AudioClip[] WalkClips;
+    public AudioClip[] RunClips;
+    public AudioSource audio;
 
-    
-    
+
+
     Vector3 last_velocity = Vector3.zero;
     bool m_IsPaused = false;
 
     float m_VerticalAngle, m_HorizontalAngle;
-
+    bool isAirborn = false;
 
     public float currentVerticalSpeed { get; private set; } = 0.0f;
 
@@ -44,7 +43,7 @@ public class Controller : MonoBehaviour
     public bool CanPause { get; set; } = true;
 
     public bool Grounded => m_Grounded;
-
+    private bool step = true; 
     CharacterController m_CharacterController;
 
     Inventory m_inventory;
@@ -52,11 +51,12 @@ public class Controller : MonoBehaviour
     bool m_Grounded;
     float m_GroundedTimer;
     bool m_crouching = false;
-
+    bool isMoving = false;
 
     void Awake()
     {
         Instance = this;
+        audio = GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
@@ -84,9 +84,7 @@ public class Controller : MonoBehaviour
     void Update()
     {
         Vector3 m_velocity = Vector3.zero;
-        bool wasGrounded = m_Grounded;
         //bool loosedGrounding = false;
-        
 
         if (!InGameMenu.GameIsPaused && !ColorPuzzleManager.IsActive())
         {
@@ -99,24 +97,28 @@ public class Controller : MonoBehaviour
             bool isRunning = Input.GetKey(KeyCode.LeftShift);
             float curSpeedX = !m_IsPaused ? (isRunning ? RunningSpeed : PlayerSpeed) * Input.GetAxis("Vertical") : 0;
             float curSpeedY = !m_IsPaused ? (isRunning ? RunningSpeed : PlayerSpeed) * Input.GetAxis("Horizontal") : 0;
-            
+            if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) isMoving = true;
+            else isMoving = false;
 
             m_velocity = (forward * curSpeedX) + (right * curSpeedY);
+           
             m_velocity.y = 0;
             if (m_CharacterController.isGrounded)
             {
                 currentVerticalSpeed = -gravity * Time.deltaTime;
-                if (Input.GetButton("Jump") )
+                if (Input.GetButton("Jump"))
                 {
                     currentVerticalSpeed = JumpSpeed;
-                    if(m_crouching)
+                    if (m_crouching)
                     {
                         m_CharacterController.height = 2.0f;
                         MainCamera.transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Time.deltaTime * crouchSmooth);
                         m_crouching = false;
                     }
+                    isAirborn = true;
+                    PlayClip("Jump");
                 }
-            } 
+            }
             else
             {
                 currentVerticalSpeed -= gravity * Time.deltaTime;
@@ -134,24 +136,24 @@ public class Controller : MonoBehaviour
             //crouching
             if (Input.GetKeyDown(KeyCode.C))
             {
-                
+
                 if (m_crouching)
                 {
 
                     m_CharacterController.height = 2.0f;
-                    MainCamera.transform.position = Vector3.Lerp(transform.position,new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z),Time.deltaTime* crouchSmooth);
+                    MainCamera.transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Time.deltaTime * crouchSmooth);
                     m_crouching = false;
                 }
                 else
                 {
                     m_crouching = true;
-                    MainCamera.transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Time.deltaTime*crouchSmooth);
+                    MainCamera.transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Time.deltaTime * crouchSmooth);
                     m_CharacterController.height = 1.0f;
 
                 }
             }
 
-           
+
 
             // Turn player
 
@@ -175,9 +177,38 @@ public class Controller : MonoBehaviour
 
 
             //currentVerticalSpeed = move.magnitude / (PlayerSpeed * Time.deltaTime);
+            if (!isAirborn && step)
+            {
+                if (isRunning)
+                {
+                    audio.clip = RunClips[Random.Range(0, RunClips.Length)];
+                    //audio.volume = 0.1f;
+                    StartCoroutine(WaitForFootSteps(0.25f));
+                    //AudioSource source = AudioManager.GetSource("Run");
+                    if (isMoving && !audio.isPlaying) audio.Play(); // if player is moving and audiosource is not playing play it
+                    if (!isMoving) audio.Stop(); // if player is not moving and audiosource is playing stop it
+
+                }
+                else
+                {
+                    audio.clip = WalkClips[Random.Range(0, WalkClips.Length)];
+                    StartCoroutine(WaitForFootSteps(0.25f));
+                    //audio.volume = 0.1f; 
+                    if (isMoving && !audio.isPlaying) audio.Play(); // if player is moving and audiosource is not playing play it
+                    if (!isMoving) audio.Stop(); // if player is not moving and audiosource is playing stop it
+                }
+            }
+            if (m_CharacterController.isGrounded)   {
+                if (isAirborn)
+                {
+                    PlayClip("Land");
+                    isAirborn = false;
+                }
+
+            }
+
             
-            
-            
+
         }
 
         else
@@ -188,9 +219,10 @@ public class Controller : MonoBehaviour
 
     }
 
-    public void PlayClip(AudioClip clip, float pitchMin, float pitchMax)
+    public void PlayClip(string clip)
     {
-        m_Source.pitch = Random.Range(pitchMin, pitchMax);
-        m_Source.PlayOneShot(clip);
+        AudioManager.Play(clip);
     }
+
+    IEnumerator WaitForFootSteps(float stepsLength) { step = false; yield return new WaitForSeconds(stepsLength); step = true; }
 }
