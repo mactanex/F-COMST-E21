4b07993d24d4facfe72dd4c1cc38b80ae872e549
Lakselord <frederik@hasselholm.dk>
2021-12-02 16:08:19 +0100

    added script to tv

--- F-COMST-Project/Assets/Scripts/tvNoise.cs
+++ F-COMST-Project/Assets/Scripts/tvNoise.cs
@@ -6,6 +6,7 @@ public class tvNoise : MonoBehaviour
 {
 
     MeshRenderer renderer;
+    AudioSource source;
     public Material blankScrene;
     public Material staticNoise;
     public Material JumpScare;
