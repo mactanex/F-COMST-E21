37405359d48258fb666fde7aed7ac58140e36e5c
Lakselord <frederik@hasselholm.dk>
2021-12-02 16:04:10 +0100

    inplemented jumpscare

---
+++ F-COMST-Project/Assets/Scripts/tvNoise.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tvNoise : MonoBehaviour
+{
+
+    MeshRenderer renderer;
+    public Material blankScrene;
+    public Material staticNoise;
+    public Material JumpScare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            var materials = renderer.sharedMaterials;
+
+            if (string.Equals(materials[1].name, "YourPic"))
+            {
+                materials[1] = staticNoise;
+            }
+
+            else if (string.Equals(materials[1].name, "tvNoise"))
+            {
+                materials[1] = JumpScare;
+            }
+
+            else if (string.Equals(materials[1].name, "jumpScare"))
+            {
+                materials[1] = blankScrene;
+            }
+
+            renderer.sharedMaterials = materials;
+        }
+    }
+}
